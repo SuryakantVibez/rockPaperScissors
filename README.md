d6ca0707336fd0babf957012074b76e204d631d7
SuryakantVibez <suryakant.yadav2007@gmail.com>
2026-02-22 22:01:17 +0530

    Buttons now work, funtion made to check every button click for result

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rockPaperScissors
+
+This project is to
+LEARN
+
+- Basic JavaScript
+- Integration of JavaScript into HTML
+- Revising Git, GitHub and how to it
+- Revisiting the beloved terminal <3
--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -7,6 +7,7 @@
 // Bot choosing an option
 
 BotChoice = Math.random();
+
 if (BotChoice >= 0 && BotChoice <= 0.33) {
   BotChoice = "r";
 } else if (BotChoice >= 0.33 && BotChoice < 0.66) {
@@ -19,39 +20,51 @@ console.log("Bot chooses " + BotChoice); // Testing
 
 // Player deciding an option
 
-document.getElementById("Rock").onclick{
+let UserChoice = null;
+
+document.getElementById("rockBtn").onclick = function () {
   UserChoice = "r";
-}
+  console.log(UserChoice);
+  printResult();
+};
+
+document.getElementById("paperBtn").onclick = function () {
+  UserChoice = "p";
+  console.log(UserChoice);
+  printResult();
+};
+
+document.getElementById("scissorBtn").onclick = function () {
+  UserChoice = "s";
+  console.log(UserChoice);
+  printResult();
+};
 
 console.log("User chooses " + UserChoice); // Testing
 
 // Check if user's option actually makes sense
 
-if (!(UserChoice == "r" || UserChoice == "p" || UserChoice == "s")) {
-  console.log("That ain't an option");
-}
-
-if (UserChoice == BotChoice) {
-  console.log("Ah... Thats a draw...");
-}
-
-// Rock-Paper possibilites
-else if (UserChoice == "r" && BotChoice == "p") {
-  console.log("Bot Won!");
-} else if (UserChoice == "p" && BotChoice == "r") {
-  console.log("You Won!");
-}
-
-// Paper-scissors possibilities
-else if (UserChoice == "p" && BotChoice == "s") {
-  console.log("Bot Won!");
-} else if (UserChoice == "s" && BotChoice == "p") {
-  console.log("You Won!");
-}
-
-// Scissors-rock possibilities
-else if (UserChoice == "s" && BotChoice == "r") {
-  console.log("Bot Won!");
-} else if (UserChoice == "p" && BotChoice == "r") {
-  console.log("You Won!");
+function printResult() {
+  
+  if (UserChoice == BotChoice) {
+    console.log("Ah... Thats a draw...");
+  }
+  // Rock-Paper possibilites
+  else if (UserChoice == "r" && BotChoice == "p") {
+    console.log("Bot Won!");
+  } else if (UserChoice == "p" && BotChoice == "r") {
+    console.log("You Won!");
+  }
+  // Paper-scissors possibilities
+  else if (UserChoice == "p" && BotChoice == "s") {
+    console.log("Bot Won!");
+  } else if (UserChoice == "s" && BotChoice == "p") {
+    console.log("You Won!");
+  }
+  // Scissors-rock possibilities
+  else if (UserChoice == "s" && BotChoice == "r") {
+    console.log("Bot Won!");
+  } else if (UserChoice == "p" && BotChoice == "r") {
+    console.log("You Won!");
+  }
 }
